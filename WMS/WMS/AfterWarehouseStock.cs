bc3dc218f72d52580d3da8d5a3dd49ef74ef81f8
MariToma <tomamaria1996@gmail.com>
2018-01-12 20:37:31 +0200

    Final-fara executabile

---
+++ WMS/WMS/AfterWarehouseStock.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WMS
+{
+    public partial class AfterWarehouseStock : Form
+    {
+
+        public AfterWarehouseStock()
+        {
+            InitializeComponent();
+            using (var context = new WMSEntities())
+            {
+                cmbWarehouse.DataSource = (from c in context.Warehouses
+                                           select c.WarehouseID
+                                               ).ToList();
+
+            }
+
+        }
+
+
+        private void btnStockW_Click(object sender, EventArgs e)
+        {
+            int warehouseID = Int32.Parse(cmbWarehouse.SelectedItem.ToString());
+            using (var context = new WMSEntities())
+            {
+                gvStockW.DataSource = (from p in context.Products
+                                       where p.WarehouseID == warehouseID
+                                       select new
+                                       {
+                                           ProductName = p.ProductName,
+                                           UnitsInStock=p.UnitsInStock
+                                            
+                                       }
+                                     ).Distinct().ToList();
+            }
+        }
+
+
+    }
+}
---
+++ WMS/WMS/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WMS
+{
+    public partial class Form1 : Form
+    {
+        DataTable dbdataset = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            AutoCompleteText();
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=.;Initial Catalog=WMS;Integrated Security=True";
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = conn;
+            command.CommandText = "Select * FROM Products ";
+
+            
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            adapter.Fill(dbdataset);
+            
+        }
+        void AutoCompleteText()
+        {
+            using (var context = new WMSEntities())
+            {
+                
+
+                Search_txt.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                Search_txt.AutoCompleteSource = AutoCompleteSource.CustomSource;
+                AutoCompleteStringCollection coll = new AutoCompleteStringCollection();
+                var test = (from p in context.Products
+                            select p.ProductName
+                                          ).Distinct().ToList();
+                foreach(var a in test)
+                {
+                    coll.Add(a);
+                }
+                Search_txt.AutoCompleteCustomSource = coll;
+            }
+
+        }
+
+        private void Search_txt_TextChanged(object sender, EventArgs e)
+        {
+            DataGridView DataGridViwe1 = new DataGridView();
+            DataView DV = new DataView(dbdataset);
+            DV.RowFilter = string.Format("ProductName LIKE '%{0}%'", Search_txt.Text);
+            DataGridViwe1.DataSource = DV;
+          
+        }
+    }
+}
