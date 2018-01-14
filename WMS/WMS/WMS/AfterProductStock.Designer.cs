6888506bdcb57bebee6a6cfa05cfb16b27427363
MariToma <tomamaria1996@gmail.com>
2018-01-14 21:26:34 +0200

    final version

---
+++ WMS/WMS/WMS/AfterProductStock.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WMS
+{
+    partial class AfterProductStock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStockP = new System.Windows.Forms.Button();
+            this.gvStockP = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Search_txt = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gvStockP)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select the product name";
+            // 
+            // btnStockP
+            // 
+            this.btnStockP.Location = new System.Drawing.Point(133, 76);
+            this.btnStockP.Margin = new System.Windows.Forms.Padding(2);
+            this.btnStockP.Name = "btnStockP";
+            this.btnStockP.Size = new System.Drawing.Size(138, 29);
+            this.btnStockP.TabIndex = 2;
+            this.btnStockP.Text = "See stock";
+            this.btnStockP.UseVisualStyleBackColor = true;
+            this.btnStockP.Click += new System.EventHandler(this.btnStockP_Click);
+            // 
+            // gvStockP
+            // 
+            this.gvStockP.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.gvStockP.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gvStockP.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvStockP.Location = new System.Drawing.Point(52, 147);
+            this.gvStockP.Margin = new System.Windows.Forms.Padding(2);
+            this.gvStockP.Name = "gvStockP";
+            this.gvStockP.RowTemplate.Height = 24;
+            this.gvStockP.Size = new System.Drawing.Size(298, 128);
+            this.gvStockP.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WMS.Properties.Resources.sistem_wms_228;
+            this.pictureBox1.Location = new System.Drawing.Point(306, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Search_txt
+            // 
+            this.Search_txt.Location = new System.Drawing.Point(211, 30);
+            this.Search_txt.Name = "Search_txt";
+            this.Search_txt.Size = new System.Drawing.Size(185, 20);
+            this.Search_txt.TabIndex = 5;
+            this.Search_txt.TextChanged += new System.EventHandler(this.Search_txt_TextChanged);
+            // 
+            // AfterProductStock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(408, 304);
+            this.Controls.Add(this.Search_txt);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.gvStockP);
+            this.Controls.Add(this.btnStockP);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "AfterProductStock";
+            this.Text = "AfterProductStock";
+            ((System.ComponentModel.ISupportInitialize)(this.gvStockP)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStockP;
+        private System.Windows.Forms.DataGridView gvStockP;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox Search_txt;
+    }
+}
---
+++ WMS/WMS/WMS/PickingForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WMS
+{
+    public partial class PickingForm : Form
+    {
+        string Product;
+        int rowID;
+        int quantity;
+        public PickingForm(string Product, int rowID,int quantity)
+        {
+            InitializeComponent();
+            this.Product = Product;
+            this.rowID = rowID;
+            this.quantity = quantity;
+            txtPicking.Text = "Picking started: ";
+            txtPicking.AppendText(Environment.NewLine);
+            int aux = quantity;
+            for (int i = 1; i <= quantity; i++)
+            {
+                int shelf;
+                if (aux % 20 == 0) shelf = (int)aux / 20;
+                else shelf = (int)aux / 20 + 1;
+                txtPicking.AppendText(String.Format("Picking {0} from row {1}, shelf {2}...", Product, rowID, shelf));
+                txtPicking.AppendText(Environment.NewLine);
+                aux--;
+            }
+            txtPicking.AppendText("Picking done!!!");
+            txtPicking.AppendText(Environment.NewLine);
+            txtPicking.AppendText(String.Format("{0} units of {1} are ready to be boarded!",quantity,Product));
+            txtPicking.AppendText(Environment.NewLine);
+            txtPicking.AppendText("Boarding...");
+            txtPicking.AppendText(Environment.NewLine);
+            txtPicking.AppendText("Boarding done! Products are ready to be delivered!");
+
+
+        }
+
+        private void txtPicking_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
